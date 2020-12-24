686bd78b802b6f36a2994dcc49cf01d476a787d4
strahinjaEmotion <S.Velickovic@emotion-home.eu>
2020-12-24 13:38:57 +0100

    Transformed to class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
