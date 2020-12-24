acbe4aba4da7608cec39d5bdc6cc6deca6943445
strahinjaEmotion <S.Velickovic@emotion-home.eu>
2020-12-24 14:39:49 +0100

    Components,props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-
+import Navbar from './components/layout/Navbar';
 class App extends Component {
   foo1 = () => 'Bars'; //class method
 
   render() {
-    const name = 'John Doe';
-    const loading = true;
-    const foo = () => 'Bar'; //function
-    const showName = true;
-
     return (
-      //must have only one parent element
-      //we can use React.Fragment(ghost element) if we don't want div to be parent element
       <div className="App">
-        <h1>
-          Hello {this.foo1()} and {showName && name}
-        </h1>
-        <h1>Hello {foo()}</h1>
+        <Navbar />
       </div>
     );
   }
