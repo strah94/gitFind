b6e1bedc60d8f23806bee56da09677cd7466a0bf
strah94 <S.Velickovic@emotion-home.eu>
2020-12-25 19:23:25 +0100

    Events and search component

---
+++ src/components/users/Search.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  onChange = (e) => {
+    //instead of setState (text:e.target.value) we can use [e.target.name]:e.target.value
+    //cause if we have multiple fields we dont wanna use email,name,lastname we can just use e.target.name
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  //if we don't use arrow functions for onSubmit
+  //we would have to bind(this) to onSubmit
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
