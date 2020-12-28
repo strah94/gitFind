ce4ccc3abf340286f5773e7dfcc86b4cd9163a4c
strah94 <S.Velickovic@emotion-home.eu>
2020-12-28 01:05:07 +0100

    React router setup

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 // props moze i
 // const Navbar=({icon,title})=>{}
@@ -11,6 +12,14 @@ const Navbar = (props) => {
         <i className={props.icon} />
         {props.title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
