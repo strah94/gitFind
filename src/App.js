f99647980e26f1724619a3e7d546518ab9b70fd9
strah94 <S.Velickovic@emotion-home.eu>
2020-12-28 17:40:13 +0100

    Converting search class to functional component with useState

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   //Search github users
@@ -39,6 +40,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  //Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   //Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -54,7 +66,7 @@ class App extends Component {
   };
 
   render() {
-    const { user, users, loading } = this.state;
+    const { user, repos, users, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -85,7 +97,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
